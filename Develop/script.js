d23e38fb001cd3155feedf12f69bbf2b499e345f
Daniel Kiyuna <danielkiyuna78640@gmail.com>
2022-03-08 02:12:08 -0600

    make the code actually work

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,42 @@
-// Assignment code here
+// Random password generator
+function generatePassword(length, prefs) {
+  var characters = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercase  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+  if (prefs.upper) { characters = characters + uppercase }
+  if (prefs.nums) { characters = characters + numbers }
+  if (prefs.symbols) { characters = characters + symbols }
+
+  var charactersLength = characters.length;
+  var result = '';
+
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+      charactersLength));
+  }
+
+  return result;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password text box
 function writePassword() {
-  var password = generatePassword();
+  var characterAmount = prompt("How long do you want the password to be?");
+
+  var useUppercase = prompt("Do you want uppercase characters (y/n)?") === "y";
+  var useNumbers = prompt("Do you want numbers (y/n)?") === "y";
+  var useSymbols = prompt("Do you want special characters (y/n)?") === "y";
+
+  var password = generatePassword(characterAmount, { upper: useUppercase, nums: useNumbers, symbols: useSymbols });
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  String.fromCharCode(65)
- }
-
- const passwordCharacters = []
- for (let i = 0; i < characterAmount; i++) {
-   const charcterCode = charCodes[Math.floor(Math.random() *
-  characterAmount)]
-  passwordCharacters.push(String.fromCharCode(charactercode))
-  }
- }
-
- function arrayFromLowToHigh(low, high) {
-   const array = []
-   for (let i = low; i <= high; i++) {
-     array.push(i)
-   }
-   return array
- }
-
- function syncCharacterAmount(e) {
-   const value = e.target.value
-   characterAmountNumber.value = value
-   characterAmountRange.value = value
- }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
